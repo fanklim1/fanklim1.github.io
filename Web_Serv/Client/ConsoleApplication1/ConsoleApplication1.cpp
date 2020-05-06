56d61fc6302f1467eaec2d4af84942285c40b231
fanklim1 <55453349+fanklim1@users.noreply.github.com>
2020-05-06 19:08:07 +0300

    Add files via upload

---
+++ Web_Serv/Client/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,48 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+
+#pragma warning(disable: 4996) // для того что бы не было ошибки inet_addr
+
+SOCKET Connection;
+
+
+
+int main(int argc, char* argv[]) {
+	//WSAStartup
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		std::cout << "Error" << std::endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	Connection = socket(AF_INET, SOCK_STREAM, NULL); // создание новго сокета для соединения с сервером 
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) { // присоединение к серверу 
+		std::cout << "Error: failed connect to server.\n";// ошибка 
+		return 1;
+	}
+	std::cout << "Connected!\n";
+
+	
+
+	system("pause");
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Web_Serv/Web_Server/Web_Server/Web_Server.cpp
@@ -0,0 +1,53 @@
+﻿#pragma comment(lib, "ws2_32.lib") // для получения некоторым функциям
+#include <winsock2.h> // библиотека для работы с сетью (2) версия 
+#include <iostream>
+
+#pragma warning(disable: 4996) 
+
+SOCKET Connections[100]; // массив сокетов для большего кол-ва клиентов 
+int Counter = 0; // перпменая с индексом для пользователя 
+
+
+
+int main(int argc, char* argv[]) {
+	//WSAStartup 
+	WSAData wsaData; //
+	WORD DLLVersion = MAKEWORD(2, 1); // запрашиваемая версия винсок 
+	if (WSAStartup(DLLVersion, &wsaData) != 0) { //для загрузки библиотеки нужно ввызвать функцию WSAStartup 1 параметр версия программы вторая ссылка на структру  
+		std::cout << "Error" << std::endl; // проврека на тот случай если бибилиотека не загрузилась 
+		exit(1);
+	}
+
+	SOCKADDR_IN addr; // инфо об адрессе сокета 
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");//   айпи адресс
+	addr.sin_port = htons(1111);// порт
+	addr.sin_family = AF_INET;// для интернет протоколов указывается константа  AF_INET
+
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL); // прослушивание на порту  2 параметр  протокол устанавливающий соединение  
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr)); // привязка к адрессу сокета  
+	listen(sListen, SOMAXCONN); // прослушивание 1 параметр сокет 2 макс кол-во запросов  
+
+	SOCKET newConnection; // сокет для нового соединения 
+	for (int i = 0; i < 100; i++) { // цикл для создания новых сокетов 
+		newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr); // возращает указание на сокет клиента 
+
+		if (newConnection == 0) {
+			std::cout << "Error #2\n"; // если соединение превано возвращает ошибку 
+		}
+		else {
+			std::cout << "Client Connected!\n"; // сообщение о том что пользователь подлючился к серверу 
+			char msg[256] = "Privet";
+			send(newConnection, msg, sizeof(msg), NULL); // сообщение которое идет клиенту 
+
+
+			Connections[i] = newConnection;
+			Counter++;
+	
+		}
+	}
+
+
+	system("pause");
+	return 0;
+}
